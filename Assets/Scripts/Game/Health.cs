d9e31dee7fd51a4dc8e5197a1878347bc2b9c214
gilecheverria <gilbertoef@gmail.com>
2021-04-09 14:56:57 -0500

    Add another character to side scrolling scene

---
+++ Assets/Scripts/Game/Health.cs
@@ -0,0 +1,23 @@
+﻿/*
+Script to control the health of players and enemies
+
+Gilberto Echeverria
+2021-04-07
+*/
+
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
