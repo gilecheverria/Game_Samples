8eb8f0c43bd2e0f3011e11737c3a6516724a0902
Gilberto Echeverria <gilbertoef@gmail.com>
2023-04-19 10:05:24 -0600

    Update Physics 2D scene

--- Assets/Scripts/Game/wwwFormGameData.cs
+++ Assets/Scripts/Game/wwwFormGameData.cs
@@ -1,3 +1,10 @@
+/*
+Scritp to get information from a DB to configure the game
+
+Gilberto Echeverria
+2023-04-19  Update the error validation for UnityWebRequest
+*/
+
 using UnityEngine;
 using UnityEngine.Networking;
 using System.Collections;
@@ -6,7 +13,7 @@ public class wwwFormGameData : MonoBehaviour
 {
     // The route for the api that inserts data.
     [SerializeField] string apiURL = "http://localhost:5000/api/gamedata";
-    // References for the scripts that hold the information that is going to be inserted.
+    // References for the scripts that hold the information to be inserted.
     [SerializeField] Points pointScript = null;
     DropBalls ballScript = null;
     Timer timeScript = null;
@@ -23,7 +30,8 @@ public class wwwFormGameData : MonoBehaviour
         // Unity sends a form, just as a html form.
         WWWForm form = new WWWForm();
 
-        // We need to create the form first, by manually adding fields. These fields match the names of the columns in the database.
+        // We need to create the form first, by manually adding fields.
+        // These fields match the names of the columns in the database.
         // The values from the other scripts is checked here and added to the form.
         form.AddField("numberBalls", ballScript.numberBalls.ToString());
         form.AddField("score", pointScript.score.ToString());
@@ -32,22 +40,18 @@ public class wwwFormGameData : MonoBehaviour
 
         Debug.Log(form);
 
-        // We create a request that makes a post to the url, and sends the form we just created.
+        // We create a request that makes a post to the url,
+        // and sends the form we just created.
         using (UnityWebRequest request = UnityWebRequest.Post(apiURL, form))
         {
-            // The yield return line is the point at which execution will pause, and be resumed after the request ends.
+            // The yield return line is the point at which execution will pause,
+            // and be resumed after the request ends.
             yield return request.SendWebRequest();
 
             // If there are no errors...
-            // This line works for Unity 2019
-            if (request.isNetworkError || request.isHttpError)
-            // Unity 2020 will likely have to use
-            //if (request.error != null)
-            {
+            if (request.result == UnityWebRequest.Result.ConnectionError) {
                 Debug.Log(request.error);
-            }
-            else
-            {
+            } else {
                 // We get the response text and log it in the console.
                 Debug.Log(request.downloadHandler.text);
                 Debug.Log("Form upload complete!");
